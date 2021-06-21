74b11052374b82e2d42b4a1501d92c384e15e953
upalinski <ulad@cere.io>
2021-06-21 12:20:21 +0300

    configure generate-load command

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
     //Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
 
     testImplementation("io.quarkus:quarkus-junit5")
 }
