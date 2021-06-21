ca7db6451be85b500e6027ab188fdba09b1713c4
upalinski <ulad@cere.io>
2021-06-21 14:18:05 +0300

    fix generate-load command

--- src/main/kotlin/network/cere/ddc/cli/picocli/GenerateLoadCommand.kt
+++ src/main/kotlin/network/cere/ddc/cli/picocli/GenerateLoadCommand.kt
@@ -17,33 +17,38 @@ class GenerateLoadCommand(private val ddcCliConfigFile: DdcCliConfigFile) : Runn
 
     @CommandLine.Option(
         names = ["-u", "--users"],
+        defaultValue = "1",
         description = ["Number of concurrent users (default - 1)"]
     )
-    var users: Int = 1
+    var users: Int? = 0
 
     @CommandLine.Option(
         names = ["-n", "--number"],
+        defaultValue = Int.MAX_VALUE.toString(),
         description = ["Number of pieces per user (default - int max value)"]
     )
-    var number: Int = Int.MAX_VALUE
+    var number: Int? = 0
 
     @CommandLine.Option(
         names = ["-i", "--interval"],
+        defaultValue = "1s",
         description = ["Interval between user requests (default - 1s)"]
     )
-    var interval: Duration = Duration.parse("1s")
+    var interval: Duration? = null
 
     @CommandLine.Option(
         names = ["-s", "--size"],
+        defaultValue = "1000",
         description = ["Piece size to be generted in bytes (default - 1000)"]
     )
-    var size: Int = 1000
+    var size: Int? = 0
 
     @CommandLine.Option(
         names = ["--profile"],
+        defaultValue = DdcCliConfigFile.DEFAULT_PROFILE,
         description = ["Configuration profile to use)"]
     )
-    var profile: String = DdcCliConfigFile.DEFAULT_PROFILE
+    var profile: String? = null
 
     override fun run() {
         val configOptions = ddcCliConfigFile.read(profile)
@@ -58,11 +63,11 @@ class GenerateLoadCommand(private val ddcCliConfigFile: DdcCliConfigFile) : Runn
         )
 
         runBlocking {
-            repeat(users) {
+            repeat(users!!) {
                 val userPubKey = UUID.randomUUID().toString()
                 launch {
-                    repeat(number) {
-                        val data = "0".repeat(size)
+                    repeat(number!!) {
+                        val data = "0".repeat(size!!)
 
                         val res = ddcProducer.send(
                             Piece(
@@ -77,7 +82,7 @@ class GenerateLoadCommand(private val ddcCliConfigFile: DdcCliConfigFile) : Runn
 
                         println("cid: ${res.cid}")
 
-                        delay(interval.toMillis())
+                        delay(interval!!.toMillis())
                     }
                 }
             }
